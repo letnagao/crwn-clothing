89e2341b1003171b4f71b0be4eac7a9367da5d7d
Leticia Lumi Nagao <leticia-nagao@hotmail.com>
2022-05-06 15:26:59 -0300

    added react router link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
+import Navigation from './routes/navigation/navigation.component';
+
 import Home from './routes/home/home.component';
 
 const Shop = () => {
@@ -9,9 +11,10 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} >
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
-        </Route>
+      </Route>
     </Routes>
   );
 };
