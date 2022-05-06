d25dd2d4d2586bcc9d1d2e6e1f62e9eba7f5e18e
Leticia Lumi Nagao <leticia-nagao@hotmail.com>
2022-05-06 14:37:29 -0300

    adding react router outlet

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 
+const Shop = () => {
+  return<h1>I am shop page</h1>;
+};
+
 const App = () => {
   return (
     <Routes>
-      <Route path='/' index element={<Home />} />
+      <Route path='/' element={<Home />} >
+        <Route path='shop' element={<Shop />} />
+        </Route>
     </Routes>
   );
 };
