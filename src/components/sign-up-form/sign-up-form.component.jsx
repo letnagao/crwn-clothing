725ee8a850c6738e176ebd5604f610018e2ea0be
Leticia Lumi Nagao <leticia-nagao@hotmail.com>
2022-05-07 09:16:05 -0300

    sign up with email and password

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import { createAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 
 const defaultFormFields = {
     displayName: '',
@@ -13,11 +13,34 @@ const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword} = formFields;
 
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    }
+
     const handleSubmit = async (event) => {
         event.preventDefault();
 
-        
-    }
+        if(password != confirmPassword) {
+            alert("passwords do not match");
+            return;
+        }
+
+        try {
+            const { user } = await createAuthUserWithEmailAndPassword(
+                email, 
+                password
+            );
+
+            await createUserDocumentFromAuth(user, { displayName });
+            resetFormFields();
+        } catch(error) {
+            if(error.code = 'auth/email-already-in-use') {
+                alert('Cannot create user, email already in use');
+            } else {
+                console.log('user creation encountered an error', error);
+        }
+        }
+    };
 
     const handleChange = (event) => {
         const { name, value } = event.target;
