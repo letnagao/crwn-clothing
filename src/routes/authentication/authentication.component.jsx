e44e65e73ef8e3bb901faf9ae1123b4bd3d5ad21
Leticia Lumi Nagao <leticia-nagao@hotmail.com>
2022-05-07 11:33:14 -0300

    custom button component

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -4,8 +4,9 @@ import {
 } from '../../utils/firebase/firebase.utils';
 
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
-    
-const SignIn = () => {
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
+
+const Authentication = () => {
 
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
@@ -15,12 +16,10 @@ const SignIn = () => {
     return (
     <div>
         <h1>Sign In Page</h1>
-        <button onClick={logGoogleUser}>
-            Sign in with Google Popup
-        </button>
+        <SignInForm />
         <SignUpForm />
     </div>
     );
 };
   
-export default SignIn;
+export default Authentication;
