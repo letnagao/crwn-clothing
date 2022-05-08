70739993f3ed43a1e3750dd33a805dd144067ac8
Leticia Lumi Nagao <leticia-nagao@hotmail.com>
2022-05-08 14:54:22 -0300

    sign in form changes

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,7 +4,8 @@ import {
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
-  createUserWithEmailAndPassword
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
 } from 'firebase/auth';
 
 import {
@@ -73,5 +74,11 @@ const firebaseConfig = {
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if(!email || !password) return;
 
-  createUserWithEmailAndPassword(auth, email, password)
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if(!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
 };
