700668fa58bb32c41d3bfe35782f22c881f6520b
Leticia Lumi Nagao <leticia-nagao@hotmail.com>
2022-05-11 20:48:41 -0300

    adding shop folder and json

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App';
-import { UserProvider } from './contexts/user.context';
+import App from "./App";
+import { UserProvider } from "./contexts/user.context";
 
-import './index.scss';
+import "./index.scss";
 
-
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 render(
   <React.StrictMode>
