06b9e31f590f1bf5e0e54cbbadf903a742c64407
Leticia Lumi Nagao <leticia-nagao@hotmail.com>
2022-05-12 00:11:21 -0300

    checkout hotfix in route

--- src/App.js
+++ src/App.js
@@ -7,14 +7,13 @@ import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 
 const App = () => {
-
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
-        <Route path='auth' element={<Checkout />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   );
