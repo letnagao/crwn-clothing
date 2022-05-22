31f14c26ef3b61c377e5b90a3699f1f28ae829f1
Leticia Lumi Nagao <leticia-nagao@hotmail.com>
2022-05-22 03:32:42 -0300

    adding react-redux selectors

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './store/store';
 
 import App from './App';
-import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
+import { store } from './store/store';
 
 import './index.scss';
 
@@ -17,13 +16,11 @@ render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <UserProvider>
-          <CategoriesProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </CategoriesProvider>
-        </UserProvider>
+        <CategoriesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoriesProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
